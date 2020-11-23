d8c164a101e02221266b99a95b326648a4ded7d6
Jeff Soriano <soriano.jeffm@gmail.com>
2020-11-23 15:33:47 -0800

    Get rid of unused components

--- src/components/PetSection.js
+++ src/components/PetSection.js
@@ -5,8 +5,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
 
 import PetCard from "./PetCard";
 import AddPetModal from "./AddPetModal";
