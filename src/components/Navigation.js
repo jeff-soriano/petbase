1c69251517895bc8b5ac1d752fd683356e891513
Jeff Soriano <soriano.jeffm@gmail.com>
2020-11-24 20:00:05 -0800

    Use name from access token

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,20 @@
 import Navbar from 'react-bootstrap/Navbar';
 import LogoutButton from "./LogoutButton";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Navigation = () => {
-    return <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Jeff's pets</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* <Button className="ml-auto" variant="danger">Log out</Button> */}
-        <LogoutButton classNames="ml-auto" />
-    </Navbar>
+    const { user } = useAuth0();
+    const { name } = user;
+
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">{name}'s pets</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* <Button className="ml-auto" variant="danger">Log out</Button> */}
+            <LogoutButton classNames="ml-auto" />
+        </Navbar>
+    );
 }
 
 export default Navigation;
