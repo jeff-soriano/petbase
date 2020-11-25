9f7a384ed9a0392060b85afb84dc47568214da80
Jeff Soriano <soriano.jeffm@gmail.com>
2020-11-25 12:56:04 -0800

    Update home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,19 @@
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
 import { Link } from "react-router-dom";
 
+import dogImg from "./dog.png"
+
 const Home = () => {
     return (
-        <>
+        <Container className="text-center">
+            <img src={dogImg} alt="dog" />
             <h1>Petbase</h1>
             <h2>A simple way to store your pet information</h2>
-            <Link to="/pets"><Button>Get started</Button></Link>
-        </>
+            <Link to="/pets"><Button size="lg">Get started</Button></Link>
+        </Container>
+        // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
     );
 }
 
