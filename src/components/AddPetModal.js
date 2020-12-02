47c851933122b1a264d3cc0a2aeb3b5c9bd70605
Jeff Soriano <soriano.jeffm@gmail.com>
2020-12-02 13:24:07 -0800

    Remove required requirement for description field

--- src/components/AddPetModal.js
+++ src/components/AddPetModal.js
@@ -45,7 +45,7 @@ const AddPetModal = ({ show, handleClose, handleSubmit }) => {
                     </Form.Group>
                     <Form.Group controlId="description">
                         <Form.Label>Description</Form.Label>
-                        <Form.Control value={description} onChange={onDescriptionChange} as="textarea" placeholder="Description" required />
+                        <Form.Control value={description} onChange={onDescriptionChange} as="textarea" placeholder="Description" />
                     </Form.Group>
                     <Form.Group controlId="image">
                         <Form.Label>Upload image</Form.Label>
