309e33691751f2653c0173c645d32528b63154d6
Jeff Soriano <soriano.jeffm@gmail.com>
2020-12-01 18:55:17 -0800

    Add footer to pet page

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,11 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Navigation from "../components/Navigation";
 import PetSection from "../components/PetSection";
 
+import './Main.css';
+
 const Main = () => {
     return (
         <Container>
             <Navigation />
             <PetSection />
+            <footer className="footer">
+                © Jeff Soriano {new Date().getFullYear()}
+            </footer>
         </Container>
     );
 }
